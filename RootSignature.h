b6d1abfec0cd74c046bbc8d38dd749e8dab3fd49
rikurikuman <k021g1447@g.neec.ac.jp>
2022-06-08 17:53:04 +0900

    ちょっぴり修正

--- RootSignature.h
+++ RootSignature.h
@@ -5,24 +5,24 @@
 
 typedef D3D12_DESCRIPTOR_RANGE DescriptorRange;
 typedef D3D12_STATIC_SAMPLER_DESC StaticSamplerDesc;
+typedef std::vector<DescriptorRange> DescriptorRanges;
+typedef std::vector<StaticSamplerDesc> StaticSamplerDescs;
 
 struct RootParamater {
     D3D12_ROOT_PARAMETER_TYPE ParameterType;
-    std::vector<D3D12_DESCRIPTOR_RANGE> DescriptorTable;
+    DescriptorRanges DescriptorTable;
     D3D12_ROOT_CONSTANTS Constants;
     D3D12_ROOT_DESCRIPTOR Descriptor;
     D3D12_SHADER_VISIBILITY ShaderVisibility;
 };
 
+typedef std::vector<RootParamater> RootParamaters;
+
 struct RootSignatureDesc {
-    std::vector<RootParamater> RootParamaters;
-    std::vector<D3D12_STATIC_SAMPLER_DESC> StaticSamplers;
+    RootParamaters RootParamaters;
+    StaticSamplerDescs StaticSamplers;
     D3D12_ROOT_SIGNATURE_FLAGS Flags;
 };
-
-typedef std::vector<DescriptorRange> DescriptorRanges;
-typedef std::vector<StaticSamplerDesc> StaticSamplerDescs;
-typedef std::vector<RootParamater> RootParamaters;
 
 class RootSignature
 {
